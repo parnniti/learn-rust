871ab70c89a664d29d780d91d15f850596a1a900
Nitipan <y06016@autox.co.th>
2024-08-20 20:09:19 +0700

    update

--- bank/src/main.rs
+++ bank/src/main.rs
@@ -15,6 +15,18 @@ impl Bank {
     fn new() -> Self {
         Bank { accounts: vec![] }
     }
+
+    fn add_account(&mut self, account: Account) {
+        self.accounts.push(account);
+    }
+    
+    fn total_balance(&self) -> i32 {
+        self.accounts.iter().map(|account| account.balance).sum()
+    }
+
+    fn summary(&self) -> Vec<String> {
+        self.accounts.iter().map(|account| account.summary()).collect()
+    }
 }
 
 impl Account {
@@ -25,23 +37,32 @@ impl Account {
             balance: 0,
         }
     }
-}
 
-fn print_account(account: &Account) {
-    println!("{:#?}", account);
-}
+    fn summary(&self) -> String {
+        format!("Account {} is held by {} with a balance of {}", self.id, self.holder, self.balance)
+    }
 
-fn change_account(account: &mut Account) {
-    account.balance = 10;
+    fn deposit(&mut self, amount: i32) -> i32 {
+        self.balance += amount;
+        self.balance
+    }
+
+    fn withdraw(&mut self, amount: i32) -> i32{
+        self.balance -= amount;
+        self.balance
+    }
 }
 
 fn main() {
-    let bank = Bank::new();
+    let mut bank = Bank::new();
     let mut account = Account::new(
         1,
         "me".to_string(),
     );
+    account.deposit(500);
+    account.withdraw(100);
 
-    change_account(&mut account);
-    println!("{:#?}", account);
+    bank.add_account(account);
+    println!("{:#?}", bank.summary());
+    println!("{}", bank.total_balance());
 }
