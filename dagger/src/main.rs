c1e4896af7d51721c947dbd416b5444c4b017db6
Nitipan <y06016@autox.co.th>
2024-08-21 00:23:51 +0700

    chore: Add base_image configuration option to dagger

--- dagger/src/main.rs
+++ dagger/src/main.rs
@@ -1,4 +1,4 @@
-mod steps;
+use shared::steps;
 mod configuration;
 use clap::Parser;
 use configuration::Configuration;
@@ -9,7 +9,8 @@ async fn main() -> eyre::Result<()> {
 
     dagger_sdk::connect(|client| async move {
         let Configuration { port, base_image } = Configuration::parse();
-        steps::utils::echo(&client, base_image, &port.to_string()).await?;
+        steps::utils::echo(&client, &base_image, &port.to_string()).await?;
+        steps::utils::echo(&client, &base_image, "Fuck you bro").await?;
         Ok(())
     })
     .await?;
--- dagger/src/steps/utils.rs
+++ dagger/src/steps/utils.rs
@@ -1,7 +1,7 @@
-use dagger_sdk::{Query};
+use dagger_sdk::Query;
 use eyre::{Result, Report};
 
-pub async fn echo(client: &Query, image: String, message: &str) -> Result<(), Report> {
+pub async fn echo(client: &Query, image: &String, message: &str) -> Result<(), Report> {
     let output = client
         .container()
         .from(image)
